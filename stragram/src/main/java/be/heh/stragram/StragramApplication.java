7926dbf0b67256a93c7b229c87cf60e5d83e0dfa
Panda <46421954+PandaDouxYT@users.noreply.github.com>
2025-10-13 15:53:50 +0200

    HEH-19: architecture hexagonale — première passe

---
+++ stragram/src/main/java/be/heh/stragram/StragramApplication.java
@@ -0,0 +1,13 @@
+package be.heh.stragram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StragramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StragramApplication.class, args);
+    }
+
+}
